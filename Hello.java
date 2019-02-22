d1a00fc6d26def750e785fec563e9772a3c6f883
prakhar <agarwal.prakhar824@gmail.com>
2019-02-22 16:30:28 +0530

    this program displays hello using method

---
+++ Hello.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class Hello
+{
+	public static void display()
+	{
+		System.out.println("Hello");
+	}
+	public static void main(String args[]) throws IOException
+	{
+		Hello h=new Hello();
+		h.display();
+	}
+}
